88083090f3aa042012251fd59b1e965e0910c7a9
Matt H <59845559+yiyuanyi@users.noreply.github.com>
2020-08-18 20:42:01 +0100

    added all if else statements for possible combinations.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # rock-paper-scissors
 make rock, paper, scissors using JS for TOP
 
-Relatively easy to make computerPlay(), however I am finding it very hard to make the playRound() function work.
+1)Relatively easy to make computerPlay(), however I am finding it very hard to make the playRound() function work.
 
-May use switch statements for cases as this may be easier format than the if else statements.
+2)May use switch statements for cases as this may be easier format than the if else statements.
+
+3)Keep if else statements. Writing all statements so that is out of the way.
--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,18 +5,27 @@ let computerChoice = choices[Math.floor(Math.random() * choices.length)];
 return computerChoice;
 }
 
-function playRound() {
-    const computerSelection = computerPlay.toUpperCase;
+function playRound(playerSelection, computerSelection) {
+    let playerSelection = prompt("Choose rock, paper or scissors:");
 
-    let playerChoice = prompt("Choose rock, paper or scissors:");
-    let playerSelection = playerChoice.toUpperCase;
-
-    if (playerSelection == "ROCK" && computerSelection == "ROCK") {
-        return "Rock vs Rock! Equal power means no winners here.";
-    } else if (playerSelection == "ROCK" && computerSelection == "PAPER") {
-        return "Oh no! You have been foiled by simple paper!";
-    } else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
-        return "You crushed thoe scissors! Well done!";
+    if (playerSelection == "rock" && computerSelection == "rock") {
+        return "Rock vs Rock! Tie.";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "Rock vs Paper. Lose.";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "Rock vs Scissors. Win!";
+    } else if (playerSelection == "paper" && computerSelection == "paper") {
+        return "Paper vs Paper! Tie.";
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "Paper vs Rock. Win!";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "Paper vs Scissors. Lose.";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "Scissors vs Rock! Lose.";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "Scissors vs Paper. Win!";
+    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        return "Scissors vs Scissors. Tie.";
     } else {
         return "Ooops. Something went wrong."
     };
