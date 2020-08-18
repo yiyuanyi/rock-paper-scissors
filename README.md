2ef44d8d1c77488cd2fbec7e16f74c0f545f1156
Matt H <59845559+yiyuanyi@users.noreply.github.com>
2020-08-18 20:25:33 +0100

    remove toUpperCase from computerPlay() function as it stopped the function working.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # rock-paper-scissors
 make rock, paper, scissors using JS for TOP
 
+Relatively easy to make computerPlay(), however I am finding it very hard to make the playRound() function work.
+
+May use switch statements for cases as this may be easier format than the if else statements.
--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,20 +2,20 @@
 function computerPlay() {
 let choices = ["Rock","Paper","Scissors"];
 let computerChoice = choices[Math.floor(Math.random() * choices.length)];
-return computerChoice.toUpperCase;
+return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
     const computerSelection = computerPlay.toUpperCase;
 
     let playerChoice = prompt("Choose rock, paper or scissors:");
     let playerSelection = playerChoice.toUpperCase;
 
-    if (playerSelection == computerSelection) {
+    if (playerSelection == "ROCK" && computerSelection == "ROCK") {
         return "Rock vs Rock! Equal power means no winners here.";
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
+    } else if (playerSelection == "ROCK" && computerSelection == "PAPER") {
         return "Oh no! You have been foiled by simple paper!";
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    } else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
         return "You crushed thoe scissors! Well done!";
     } else {
         return "Ooops. Something went wrong."
